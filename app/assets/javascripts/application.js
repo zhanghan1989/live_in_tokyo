f7afdad3d3755074d2c7984c6358f98bc7a3f8a6
zhanghan1989 <dfhh198902@yahoo.co.jp>
2017-02-20 08:36:32 +0900

    index.html finished

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require vendor/jquery.anystretch.min
 //= require turbolinks
 //= require_tree .
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    @issues  = [ { title: "Ruby lovers", comments: "4" }, 
+      { title: "Rails and Laravel", comments: "5" } ]
   end
 
   def help
