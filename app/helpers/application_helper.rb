a54b31b92fe3c6780fef557bcf299c7dfcc3a735
zhanghan1989 <dfhh198902@yahoo.co.jp>
2017-02-25 11:54:13 +0900

    markdown finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def markdown(text)
+  renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+  options = {
+    autolink: true,
+    no_intra_emphasis: true,
+    fenced_code_blocks: true,
+    lax_html_blocks: true,
+    strikethrough: true,
+    superscript: true
+  }
+  Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
 end
