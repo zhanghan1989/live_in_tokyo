6201aae582097b96b4b046683e81a8cae3b35c49
zhanghan1989 <dfhh198902@yahoo.co.jp>
2017-02-26 11:11:58 +0900

    base_title finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Welcome to the 51yue.jp !"
     if page_title.empty?
       base_title
     else
