639397c5f7efc82aabda91497da57c47628dc3d6
zhanghan1989 <dfhh198902@yahoo.co.jp>
2017-03-05 21:57:30 +0900

    password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   
   has_many :comments
   has_many :issues
@@ -72,6 +72,23 @@ class User < ActiveRecord::Base
   # 发送激活邮件
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # 设置密码重设相关的属性
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # 发送密码重设邮件
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # 如果密码重设请求超时了，返回 true
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305024446) do
+ActiveRecord::Schema.define(version: 20170305123054) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20170305024446) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
