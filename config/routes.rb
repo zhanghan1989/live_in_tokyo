b09fd501c1449d1e539f1abfc6d59c94d671c116
zhanghan1989 <dfhh198902@yahoo.co.jp>
2017-03-05 20:16:31 +0900

    email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'comments/create'
 
   get 'sessions/new'
@@ -39,7 +43,7 @@ Rails.application.routes.draw do
   resources :comments, only:[:create]
 
   resources :account_activations, only: [:edit]
-
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
