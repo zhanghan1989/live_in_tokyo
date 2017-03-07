57be774df49b9f13ae90f720b4d13f1e14ce7426
zhanghan1989 <dfhh198902@yahoo.co.jp>
2017-03-08 06:57:38 +0900

    ssl fails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,19 +2,19 @@ class StaticPagesController < ApplicationController
   def home
     #@issues  = [ { title: "Ruby lovers", comments: "4" }, 
       #{ title: "Rails and Laravel", comments: "5" } ]
-    @issues = Issue.all.reverse
-  end
+      @issues = Issue.all.reverse
+    end
 
-  def help
-  end
+    def help
+    end
 
-  def about
-  end
+    def about
+    end
 
-  def contact
-  end
+    def contact
+    end
 
-  def letsencrypt
-    render text: "0c37N0E2GH_wXZOlZGIXYE8UOH4uJQi6V9XJlxBe4i8.jSnYGXTvSCXx7AwLpjYYeGtIDS5dcEl_2wjnFsJe6hs"
-  end
-end
+    def letsencrypt
+     render text: "faN8XepsttirFwB1Dh4Jhp8vPUIbIpH0QVZbNLoDYLE.jSnYGXTvSCXx7AwLpjYYeGtIDS5dcEl_2wjnFsJe6hs"
+   end
+ end
