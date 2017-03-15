63d218edc58cfa7899978fb0729e0a6dafc72a33
zhanghan1989 <dfhh198902@yahoo.co.jp>
2017-03-16 07:01:20 +0900

    bug001

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
     #@issues  = [ { title: "Ruby lovers", comments: "4" }, 
       #{ title: "Rails and Laravel", comments: "5" } ]
       @issues = Issue.all
+      # @users = User.all
     end
 
     def help
